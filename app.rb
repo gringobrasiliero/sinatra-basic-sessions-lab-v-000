8591ac8a38286a633ea0e03897bd393fec022de6
Nolan Byrnes <nolanehs@gmail.com>
2018-01-18 14:40:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ class App < Sinatra::Base
 
 configure do
   enable :sessions,
-  set :session_secret, "howdy"
-
+  set :session_secret, "secret"
 end
 
   get '/' do
     erb :index
+end
 
   post '/checkout' do
-
+    session[item] = params[:item]
+    @session = session
     erb :checkout
       end
   end
